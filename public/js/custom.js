bf01d266adaa5af1f21739d061316c9998c4bd52
gasolin <gasolin@gmail.com>
2013-03-23 15:13:52 +0800

    custom style and add result instead of alert

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,6 +2,11 @@
 
 var calc = document.getElementById('calc_bmi');
 var bmiform = document.getElementById('bmi_form');
+var result = document.getElementById('bmi_result');
+var suggest = document.getElementById('bmi_suggest');
+// var status = document.getElementById('status');
+
+
 calc.addEventListener('click', function(e) {
 	e.preventDefault();
 	calculateBmi();
@@ -11,7 +16,9 @@ function calculateBmi() {
 	var height = parseFloat(bmiform.height.value);
 	var weight = parseFloat(bmiform.weight.value);
 	if(weight > 0 && height > 0){
-		var result = weight/(height/100 * height/100);
-		alert(result.toFixed(2));
+		var calc_result = weight/(height/100 * height/100);
+		console.log(calc_result.toFixed(2));
+		result.innerHTML = 'Your BMI is ' + calc_result.toFixed(2);
+        // status.innerHTML = calc_result.toFixed(2);
 	}
 }
