1419480e85ee5f711865124a1f48970ea8ed1836
gasolin <gasolin@gmail.com>
2013-07-08 22:39:22 +0800

    add indexedDB history support

--- public/js/custom.js
+++ public/js/custom.js
@@ -6,8 +6,10 @@ var BMICalc = {
     this.getAllElements();
 
     this.calc_bmi.addEventListener('click', this.calculateBmi.bind(this));
-    this.about.addEventListener('click', this.aboutPage);
+    // this.menu_about.addEventListener('click', this.aboutPage);
 
+    this.menu_calc.addEventListener('click', this.switchView.bind(this));
+    this.menu_history.addEventListener('click', this.switchView.bind(this));
   },
 
   toCamelCase: function toCamelCase(str) {
@@ -19,8 +21,9 @@ var BMICalc = {
   getAllElements: function browser_getAllElements() {
 
     var elementIDs = [
+      'main_page', 'history_page',
       'calc_bmi', 'bmi_form', 'bmi_result', 'bmi_suggest',
-      'about'];
+      'menu_about', 'menu_calc', 'menu_history'];
 
     // Loop and add element with camel style name to Modal Dialog attribute.
     elementIDs.forEach(function createElementRef(name) {
@@ -41,6 +44,10 @@ var BMICalc = {
     if (weight > 0 && height > 0) {
       var BMI = this.get_bmi_value(height, weight);
       bmi_result.innerHTML = 'Your BMI is ' + BMI;
+      // Store result
+      History.putRecord({'bmi': BMI, 'timestamp' : new Date().getTime()},
+        null
+      );
       // Give health advice
       if (BMI > 25) {
         bmi_suggest.innerHTML = '你該節食了';
@@ -56,7 +63,39 @@ var BMICalc = {
   },
 
   aboutPage: function about_page() {
-    alert('ooxx');
+    alert('BMI webapp');
+  },
+
+  switchView: function switch_view() {
+    if (history_page.classList.contains('hidden')) {
+      this.main_page.classList.add('hidden');
+      this.history_page.classList.remove('hidden');
+      this.menu_history.classList.add('hidden');
+      this.menu_calc.classList.remove('hidden');
+      var bmi_history = document.getElementById('bmi_hisory');
+      // clean up
+      bmi_history.innerHTML = '';
+      // build
+      History.getList(function(items) {
+        // console.log(items);
+        for (var i = 0, len = items.length; i < len; i++) {
+          var li = document.createElement('li');
+          li.id = items[i].id;//alert(items[i].id);
+          var day = new Date(items[i].timestamp);
+          var logtime = (day.getYear() + 1900) + '/' +
+                        (day.getMonth() + 1) +
+                        '/' + day.getDate();
+          li.innerHTML = '<p>' + items[i].bmi + '</p>' +
+              '<p><time date="' + logtime + '">' + logtime + '</time></p>';
+          bmi_history.appendChild(li);
+        }
+      });
+    } else {
+      this.history_page.classList.add('hidden');
+      this.main_page.classList.remove('hidden');
+      this.menu_history.classList.remove('hidden');
+      this.menu_calc.classList.add('hidden');
+    }
   }
 };
 
