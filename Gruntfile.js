fc77504ab9e6c037964144e1b920bd3c1e5d0175
gasolin <gasolin@gmail.com>
2013-07-19 22:23:33 +0800

    appcache gen

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,35 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     manifest: {
       generate: {
-
+        options: {
+          basePath: "./public/",
+          // cache: ["js/app.js", "css/style.css"],
+          // cachePrefix: "",
+          // network: ["http://*", "https://*"],
+          // fallback: ["/ /offline.html"],
+          // exclude: ["js/jquery.min.js"],
+          // preferOnline: true,
+          verbose: false,
+          timestamp: true
+        },
+        src: [
+            "*.html",
+            "js/*.js",
+            "style/*.css",
+            "style/icons/*.ico",
+            "style/icons/*.png",
+            "style/images/*.png",
+            "style/images/*.jpg",
+            'style/icons/*.ico',
+            "style_unstable/*.css",
+            "style_unstable/images/*.png",
+            "style_unstable/images/*.jpg",
+            "style/**/images/ui/*.png",
+            "style/**/images/icons/*.png",
+            "lib/font-awesome/css/*.css",
+            "lib/font-awesome/font/*.*"
+        ],
+        dest: "public/manifest.appcache"
       }
     }
   });
